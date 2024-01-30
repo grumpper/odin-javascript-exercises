bcdfc3cf6d69634f5a769be8f8bfb7d73a7f26b6
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-01-30 11:26:44 +0200

    do exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (givenArray, ...givenValues) {
+    let result = []
+    for (let element of givenArray) {
+        if (!givenValues.includes(element)) {
+            result.push(element)
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
