96e0f8e5dd4e66e8e5573e55ef3befefe95ce114
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-01-30 17:22:17 +0200

    do exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (arg1, arg2) {
+    for (let arg of [arg1, arg2]) {
+        if (arg < 0 || typeof arg !== "number") {
+            return "ERROR"
+        }
+    }
+    let start, stop, result = 0
+    if (arg2 >= arg1) {
+        start = arg1
+        stop = arg2
+    } else {
+        start = arg2
+        stop = arg1
+    }
+    for (let i = start; i <= stop; i++) {
+        result += i
+    }
+    return result
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+sumAll(-5, "test")
