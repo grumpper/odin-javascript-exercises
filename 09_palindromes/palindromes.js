bf172dffc076315bd299428c404210d1761036e8
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-06 00:07:05 +0200

    do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (expression) {
+    const punctuation = [" ", ".", ",", "!", "?", ":", ";", "`", "'"]
+    const strippedExpression = []
+    for (let i = 0; i < expression.length; i++) {
+        if (!punctuation.includes(expression[i])) {
+            strippedExpression.push(expression[i].toLowerCase())
+        }
+    }
+    const reversedExpression = strippedExpression.toReversed()
+    for (let i = 0; i < expression.length; i++) {
+        if (strippedExpression[i] !== reversedExpression[i]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+palindromes("ZZZZ car, a man, a maracaz.")
