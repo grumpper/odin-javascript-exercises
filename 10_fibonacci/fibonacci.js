595d90e3b211951a7366826b7245e624431407fa
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-06 01:02:16 +0200

    do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    const intIndex = parseInt(index)
+    if (intIndex < 0) {
+        return "OOPS"
+    }
+    const sequence = []
+    for (let i = 0; i <= intIndex; i++) {
+        if (i < 2) {
+            sequence.splice(i, 0, i)
+        } else {
+            sequence.splice(i, 0, sequence[i - 1] + sequence[i - 2])
+        }
+    }
+    return sequence[intIndex]
 };
 
 // Do not edit below this line
