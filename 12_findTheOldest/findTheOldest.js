d0712ccd10ac06dc8b548fb176b90af76d3230ad
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2024-02-06 01:31:54 +0200

    do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    const thisYear = new Date().getFullYear()
+    const sortedArray = array.sort((a, b) => {
+        const aLived = "yearOfDeath" in a ? a.yearOfDeath - a.yearOfBirth : thisYear - a.yearOfBirth
+        const bLived = "yearOfDeath" in b ? b.yearOfDeath - b.yearOfBirth : thisYear - b.yearOfBirth
+        return aLived > bLived ? -1 : 1
+    })
+    return sortedArray[0]
 };
 
 // Do not edit below this line
